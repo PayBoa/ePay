e5569c47cb5ab4c02aab847c9c84439fd183849c
PayBoa <pay.borhani@gmail.com>
2025-02-18 18:40:15 +0100

    created winner in Listing model

--- auctions/models.py
+++ auctions/models.py
@@ -13,9 +13,10 @@ class Listing(models.Model):
     current_price = models.DecimalField(max_digits=10, decimal_places=2, default=starting_bid)
     image_url = models.URLField(blank=True, null=True, default="https://st4.depositphotos.com/14953852/24787/v/450/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg")
     category = models.CharField(max_length=64)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    winner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True, related_name="winner")
 
     def __str__(self):
         return f"{self.title} - {self.current_price} - by {self.owner}"
